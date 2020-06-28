533c92c5a4afb8f49cbb4735af65c9c450036024
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-06-28 14:39:27 -0300

    feat(firebase): Instala e configura o firebase

--- src/main.js
+++ src/main.js
@@ -2,10 +2,13 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import FirebaseVue from './firebase';
 
 import './assets/scss/app.scss';
 
 Vue.config.productionTip = false;
+
+Vue.use(FirebaseVue);
 
 new Vue({
   router,
