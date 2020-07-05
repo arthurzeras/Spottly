2529a0a619857ef7a793e8409a553dfc64452a8d
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-07-05 19:48:06 -0300

    feat(schedule-twitter): Implementa função scheduler para postagem no twitter

--- firebase/functions/index.js
+++ firebase/functions/index.js
@@ -1,5 +1,9 @@
 const axios = require('axios');
+const admin = require('firebase-admin');
 const functions = require('firebase-functions');
+const localFunctions = require('./local-functions');
+
+admin.initializeApp();
 
 exports.spotifyAuthorize = functions.https.onCall(async (params) => {
   try {
@@ -37,25 +41,8 @@ exports.spotifyAuthorize = functions.https.onCall(async (params) => {
 
 exports.spotifyRefreshToken = functions.https.onCall(async (params) => {
   try {
-    const data = new URLSearchParams();
-    data.append('grant_type', 'refresh_token');
-    data.append('refresh_token', params.refreshToken);
-
-    const { id, secret } = functions.config().spotify;
-    const clientCode = Buffer.from(`${id}:${secret}`).toString('base64');
-
-    const payload = {
-      data,
-      method: 'POST',
-      url: 'https://accounts.spotify.com/api/token',
-      headers: {
-        Authorization: `Basic ${clientCode}`,
-      },
-    };
-
-    const response = await axios(payload);
-
-    return response.data;
+    const data = await localFunctions.spotifyRefreshToken(params, functions.config().spotify);
+    return data;
   } catch (error) {
     const status = error.response ? error.response.status : 500;
     const code = status === 400 ? 'invalid-argument' : 'internal';
@@ -64,4 +51,41 @@ exports.spotifyRefreshToken = functions.https.onCall(async (params) => {
 
     throw new functions.https.HttpsError(code, message, detail);
   }
+});
+
+exports.postScheduler = functions.pubsub.schedule('every 24 hours').onRun(async () => {
+  const days = ['sunday', 'monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday'];
+  const today = new Date().getDay();
+
+  const ref = admin.database().ref('users');
+  const snapshot = await ref.once('value');
+
+  const users = snapshot.val();
+
+  Object.keys(users).forEach(async (user) => {
+    const current = users[user];
+
+    if (current.twitterActive && days[today] === current.postDay) {
+      try {
+        const artists = await localFunctions.getSpotifyTopArtists(
+          current,
+          functions.config().spotify
+        );
+
+        const twitterConfig = {
+          artists,
+          credentials: {
+            consumer_key: functions.config().twitter.key,
+            consumer_secret: functions.config().twitter.secret,
+            access_token_secret: current.credentials.twitter.secret,
+            access_token_key: current.credentials.twitter.accessToken,
+          },
+        };
+
+        await localFunctions.twitterPostTopArtists(twitterConfig);
+      } catch (error) {
+        console.log('Erro ao postar top artistas: ', error);
+      }
+    }
+  });
 });
---
+++ firebase/functions/local-functions.js
@@ -0,0 +1,77 @@
+const axios = require('axios');
+const Twitter = require('twitter-lite');
+
+async function spotifyRefreshToken(params, credentials) {
+  try {
+    const data = new URLSearchParams();
+    data.append('grant_type', 'refresh_token');
+    data.append('refresh_token', params.refreshToken);
+
+    const { id, secret } = credentials;
+    const clientCode = Buffer.from(`${id}:${secret}`).toString('base64');
+
+    const payload = {
+      data,
+      method: 'POST',
+      url: 'https://accounts.spotify.com/api/token',
+      headers: {
+        Authorization: `Basic ${clientCode}`,
+      },
+    };
+
+    const response = await axios(payload);
+
+    return Promise.resolve(response.data);
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+async function getSpotifyTopArtists(user, credentials, updatedAccessToken = null) {
+  const { accessToken, refreshToken } = user.credentials.spotify;
+
+  try {
+    const params = {
+      url: 'https://api.spotify.com/v1/me/top/artists',
+      params: {
+        limit: 5,
+        time_range: 'short_term',
+      },
+      headers: {
+        Authorization: `Bearer ${updatedAccessToken || accessToken}`,
+      },
+    };
+
+    const { data } = await axios(params);
+
+    return Promise.resolve(data.items);
+  } catch (error) {
+    if (error.response && error.response.status === 401) {
+      const data = await spotifyRefreshToken({ refreshToken }, credentials);
+      return getSpotifyTopArtists(user, credentials, data.access_token);
+    }
+
+    return Promise.reject(error);
+  }
+}
+
+async function twitterPostTopArtists(config) {
+  try {
+    const client = new Twitter(config.credentials);
+    const artists = config.artists.map((a) => `- ${a.name}`).join('\n');
+
+    await client.post('statuses/update', {
+      status: `Meus top 5 artistas do Spotify na semana: \n ${artists} \n via #Spottly`,
+    });
+
+    return Promise.resolve();
+  } catch (error) {
+    return Promise.reject(error);
+  }
+}
+
+module.exports = {
+  spotifyRefreshToken,
+  getSpotifyTopArtists,
+  twitterPostTopArtists,
+};
