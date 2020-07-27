78349988809cb82ec120eab3821e074dff4b4ea1
Arthur Oliveira <arthurolvmorais@gmail.com>
2020-07-27 18:03:40 -0300

    feat(top-tracks): Adiciona visualização de top musicas

--- src/services/index.js
+++ src/services/index.js
@@ -9,6 +9,15 @@ const spotify = {
         Authorization: `Bearer ${localStorage.getItem('spotify_token')}`,
       },
     }),
+
+  topTracks: (params) =>
+    axios({
+      params,
+      url: 'https://api.spotify.com/v1/me/top/tracks',
+      headers: {
+        Authorization: `Bearer ${localStorage.getItem('spotify_token')}`,
+      },
+    }),
 };
 
 export default {
