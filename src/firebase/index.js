1455403aed3adf85ef2532ebcfad9dfff30382f3
Arthur Oliveira <arthurolvmorais@gmail.com>
2021-05-12 20:47:21 -0300

    feat(save-firestore): create user on firestore when login

--- src/firebase/index.js
+++ src/firebase/index.js
@@ -1,6 +1,7 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 import 'firebase/database';
+import 'firebase/firestore';
 import 'firebase/functions';
 
 export const firebaseApp = firebase.initializeApp({
